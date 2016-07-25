0652982d3dea5fd4483ad87d73e1aa00b4725240
Fabien-Chouteau <fabien.chouteau@gmail.com>
2016-07-25 04:08:37 +0200

    Add button to trigger printing

--- src/main.adb
+++ src/main.adb
@@ -5,9 +5,29 @@ with Greyscale_And_Print; use Greyscale_And_Print;
 
 with Last_Chance_Handler;
 pragma Unreferenced (Last_Chance_Handler);
+with STM32.GPIO; use STM32.GPIO;
+with STM32.Device; use STM32.Device;
 
 procedure Main is
+   Button : GPIO_Point renames OpenMV.Shield_ADC;
+
+   procedure Init_Button;
+
+   procedure Init_Button is
+      Conf : GPIO_Port_Configuration;
+   begin
+      Enable_Clock (Button);
+
+      Conf.Mode        := Mode_In;
+      Conf.Output_Type := Push_Pull;
+      Conf.Speed       := Speed_100MHz;
+      Conf.Resistors   := Pull_Up;
+
+      Configure_IO (Button, Conf);
+   end Init_Button;
+
 begin
+   Init_Button;
    OpenMV.Initialize_LEDs;
    OpenMV.Set_RGB_LED (OpenMV.Off);
    OpenMV.LCD_Shield.Initialize;
@@ -26,6 +46,8 @@ begin
       OpenMV.LCD_Shield.Display;
 
       --  Print it
-      --  Print (OpenMV.LCD_Shield.Get_Bitmap);
+      if not Button.Set then
+         Print (OpenMV.LCD_Shield.Get_Bitmap);
+      end if;
    end loop;
 end Main;
