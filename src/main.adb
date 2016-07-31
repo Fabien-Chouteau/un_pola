48b04b132e5dd2c56c00b28a8a49371adf4b50f6
Fabien-Chouteau <fabien.chouteau@gmail.com>
2016-07-31 14:32:08 +0200

    Add mode selection (greyscale vs edge detect)

--- src/main.adb
+++ src/main.adb
@@ -1,7 +1,6 @@
 with OpenMV;
 with OpenMV.LCD_Shield;
 with OpenMV.Sensor;
-with OpenMV.Bitmap;
 
 with Greyscale_And_Print; use Greyscale_And_Print;
 with Edge_Detect;
@@ -11,28 +10,28 @@ pragma Unreferenced (Last_Chance_Handler);
 
 with STM32.GPIO; use STM32.GPIO;
 with STM32.Device; use STM32.Device;
-with HAL.Bitmap;
 
 procedure Main is
-   Button : GPIO_Point renames OpenMV.Shield_ADC;
+
+   Trigger_Button : GPIO_Point renames OpenMV.Shield_ADC;
+   Mode_Button : GPIO_Point renames OpenMV.Shield_SWC;
 
    procedure Init_Button;
 
    procedure Init_Button is
       Conf : GPIO_Port_Configuration;
    begin
-      Enable_Clock (Button);
+      Enable_Clock (Trigger_Button);
+      Enable_Clock (Mode_Button);
 
       Conf.Mode        := Mode_In;
       Conf.Output_Type := Push_Pull;
       Conf.Speed       := Speed_100MHz;
       Conf.Resistors   := Pull_Up;
 
-      Configure_IO (Button, Conf);
+      Configure_IO (Trigger_Button, Conf);
+      Configure_IO (Mode_Button, Conf);
    end Init_Button;
-
-   Snapshot : constant HAL.Bitmap.Bitmap_Buffer'Class := OpenMV.Bitmap.Allocate;
-   Do_Edge : constant Boolean := True;
 begin
    Init_Button;
 
@@ -48,30 +47,26 @@ begin
    Greyscale_And_Print.Initialize;
 
    loop
-      if Do_Edge then
-         --  Take a snapshot
-         OpenMV.Sensor.Snapshot (Snapshot);
+      --  Take a snapshot
+      OpenMV.Sensor.Snapshot (OpenMV.LCD_Shield.Get_Bitmap);
 
+
+      if not Mode_Button.Set then
          --  Convert it to grayscale
-         Greyscale_And_Print.To_Greyscale (Snapshot);
+         Greyscale_And_Print.To_Greyscale (OpenMV.LCD_Shield.Get_Bitmap);
 
-         Edge_Detect.Sobel (Snapshot, OpenMV.LCD_Shield.Get_Bitmap);
-
+         Edge_Detect.Sobel (OpenMV.LCD_Shield.Get_Bitmap);
       else
-         --  Take a snapshot
-         OpenMV.Sensor.Snapshot (OpenMV.LCD_Shield.Get_Bitmap);
-
-         --  Convert it to grayscale
+         --  Convert it to grayscale with threshold
          Greyscale_And_Print.To_Greyscale (OpenMV.LCD_Shield.Get_Bitmap,
                                            Apply_Threshol => True);
-
       end if;
 
       --  Show it on the screen
       OpenMV.LCD_Shield.Display;
 
       --  Print it
-      if not Button.Set then
+      if not Trigger_Button.Set then
          Print (OpenMV.LCD_Shield.Get_Bitmap);
       end if;
    end loop;
